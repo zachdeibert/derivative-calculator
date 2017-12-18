2730ae5e850397aedd3baad38d913cd3872d3f3d
Zach Deibert <zachdeibert@gmail.com>
2017-12-18 16:05:03 -0600

    Created project

---
+++ Entry.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Com.GitHub.ZachDeibert.DerivativeCalculator {
+    static class Entry {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
